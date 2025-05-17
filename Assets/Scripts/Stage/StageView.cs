65ab510e2debd3af44faf31ae27632823547d779
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-05-17 13:20:44 +0900

    StageViewの準備

--- Assets/Scripts/Stage/StageView.cs
+++ Assets/Scripts/Stage/StageView.cs
@@ -4,6 +4,12 @@ public class StageView : MonoBehaviour
 {
     public void Initialize()
     {
-        
+
+    }
+    public void Move(float x)
+    {
+        Vector3 newPosition = this.transform.position;
+        newPosition.x = x;
+        this.transform.position = newPosition;
     }
 }
