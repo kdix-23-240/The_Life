75d4666dd13e9e43738e502c69e1b09469bf5556
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-05-18 04:56:44 +0900

    命の画像を3枚インポート

---
+++ Assets/Scripts/GameState/GameStateModel.cs
@@ -0,0 +1,39 @@
+using UniRx;
+
+/// <summary>
+/// シングルトンでゲームの状態を管理するクラス
+/// </summary>
+public class GameStateModel
+{
+    // シングルトンインスタンス
+    private static GameStateModel _instance;
+    public static GameStateModel Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameStateModel();
+            }
+            return _instance;
+        }
+    }
+
+    public enum GameStateEnum
+    {
+        Playing,
+        Paused,
+        GameOver
+    }
+
+    private ReactiveProperty<GameStateEnum> _gameState = new ReactiveProperty<GameStateEnum>(GameStateEnum.Playing);
+
+    public IReadOnlyReactiveProperty<GameStateEnum> GameState => _gameState;
+
+    public void ChangeGameState(GameStateEnum state)
+    {
+        _gameState.Value = state;
+    }
+    // コンストラクタをprivateにして外部からの生成を禁止
+    private GameStateModel() { }
+}
--- Assets/Scripts/Hitable/GameOver.cs
+++ Assets/Scripts/Hitable/GameOver.cs
@@ -12,13 +12,7 @@ public class GameOver : IHitable
     }
     public void Hit(Collider2D other)
     {
-        // y軸方向の速度を0にする
-        Vector2 velocity = _rigidbody.linearVelocity;
-        velocity.y = 0;
-        _rigidbody.linearVelocity = velocity;
-
-        // y軸方向に力を加える
-        Vector2 force = new Vector2(0, 20f); // 上方向の力
-        _rigidbody.AddForce(force, ForceMode2D.Impulse);
+        // GameStateModel.Instance.ChangeGameState(GameStateModel.GameStateEnum.GameOver);
+        Debug.Log("GameOver");
     }
 }
